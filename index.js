a526a137cb9867c585a19351f9786d0572524aaf
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-28 22:17:51 -0500

    organize

--- index.js
+++ index.js
@@ -1,84 +1,21 @@
+////////////////
+//  index.js  //
+////////////////
+
 // Import
+const Joi = require("joi");
+const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
-const Joi = require("joi");
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-const genres = [
-  { id: 1, name: "Science Fiction" },
-  { id: 2, name: "Action" },
-  { id: 3, name: "Comedy" },
-  { id: 4, name: "Drama" },
-];
+app.use("/api/genres", genres);
 
 // Home
-app.get("/", (req, res) => {
-  res.send("Action Video");
-});
-
-// GET genres
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-
-// POST genre
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-// PUT genre
-app.put("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-// DELETE genre
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
-
-// GET genre
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-  res.send(genre);
-});
-
-// Validate f(x)
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-}
+// app.get("/", (req, res) => {
+//   res.send("Action Video");
+// });
 
 // Listener
 const port = process.env.PORT || 3000;
