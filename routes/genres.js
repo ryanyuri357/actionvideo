939b163ac97209af22624c77a36fd555d9db9818
ryanyuri357 <ryan.yuri357@gmail.com>
2021-10-03 10:10:51 -0500

    Post route

--- routes/genres.js
+++ routes/genres.js
@@ -36,28 +36,32 @@ router.get("/", async (req, res) => {
 });
 
 // POST genre
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+
   res.send(genre);
 });
 
 // PUT genre
-router.put("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  genre.name = req.body.name;
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
+
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
   res.send(genre);
 });
 
