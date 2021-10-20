3176668d313860f3686dc9f0d588bea4b668d4ea
ryanyuri357 <ryan.yuri357@gmail.com>
2021-10-20 16:05:36 -0500

    Add Users Route

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const express = require("express");
 const app = express();
 
@@ -25,6 +26,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 // Home
 app.get("/", (req, res) => {
