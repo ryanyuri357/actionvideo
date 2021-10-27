9add0f4278d9d1aa2c2021664d34a6e7869e4105
ryanyuri357 <ryan.yuri357@gmail.com>
2021-10-27 17:08:45 -0500

    Install Helmet and Compress

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation");
+require("./startup/prod")(app);
 
 // throw new Error("something failed during startup");
 
