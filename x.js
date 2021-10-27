e89f90303d769a2ed34b7ec824a5a88411599fd7
ryanyuri357 <ryan.yuri357@gmail.com>
2021-10-26 23:58:28 -0500

    Test Driven Development Setup

---
+++ x.js
@@ -0,0 +1,12 @@
+// POST /api/returns { customerId, movieId }
+
+// Return 401 if client is not logged in
+// Return 400 if customer ID is not provided
+// Return 400 if movie ID is not provided
+// Return 404 is no rental found for this customer/movie
+// Return 400 if rental already processed
+// Return 200 if valid request
+// Set the return date
+// Calculate the rental fee
+// Increase the stock
+// Return the rental
